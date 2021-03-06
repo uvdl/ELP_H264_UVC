ffb5eabfcaf129e0147de6b0a73e99f2fa88b041
waddle77 <waddle77@gmail.com>
2021-03-06 18:38:55 -0500

    Patch file applied

--- Linux_UVC_TestAP/H264_UVC_TestAP.c
+++ Linux_UVC_TestAP/H264_UVC_TestAP.c
@@ -143,8 +143,8 @@ static int CheckKernelVersion(void)
 	else
 	{
 		TestAp_Printf(TESTAP_DBG_ERR, "your kernel version: 0x%x \nTestAP support kernel version: 0x%x\n",kernelRelease, LINUX_VERSION_CODE);
-		return false;
 	}
+	return true;
 
 }
 static int GetFreeRam(int* freeram)
@@ -1858,6 +1858,7 @@ int main(int argc, char *argv[])
 			break;
 
 		case OPT_H264_IFRAME_SET:
+			TestAp_Printf(TESTAP_DBG_ERR, "IFrame chosen\n");
 			h264_iframe_reset = atoi(optarg);
 			do_h264_iframe_set = 1;
 			break;
@@ -2119,6 +2120,11 @@ int main(int argc, char *argv[])
 	{
 		if(XU_H264_Set_BitRate(dev, m_BitRate) < 0 )
 			TestAp_Printf(TESTAP_DBG_ERR, "RERVISION_UVC_TestAP @main : XU_H264_Set_BitRate Failed\n");
+	}
+
+	if(do_h264_iframe_set)
+	{
+		XU_H264_Set_IFRAME(dev);
 	}
 
 	if(do_xu_set)
